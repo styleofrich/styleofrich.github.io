0d59decf571d5a36112872c0771397d06a8d4273
styleofrich <103873099+styleofrich@users.noreply.github.com>
2022-04-17 14:03:13 +0800

    Add files via upload

---
+++ data.source.js
@@ -0,0 +1,180 @@
+import React from 'react';
+export const Banner01DataSource = {
+  wrapper: { className: 'banner0 l21y3ioai3t-editor_css' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title l21yajas5r9-editor_css',
+    children: 'https://s1.ax1x.com/2022/04/16/Lt2CJs.jpg',
+  },
+  content: {
+    className: 'banner0-content l21yap8gn7b-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <span>
+                <span>
+                  <b>
+                    <h2>
+                      <br />
+                    </h2>
+                  </b>
+                </span>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  button: {
+    className: 'banner0-button l2203jtpeth-editor_css',
+    children: 'Learn More',
+  },
+};
+export const Banner23DataSource = {
+  wrapper: { className: 'banner2' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        page: { className: 'home-page banner2-page' },
+        textWrapper: { className: 'banner2-text-wrapper' },
+        bg: { className: 'bg bg0 l2217t2dry-editor_css' },
+        title: {
+          className: 'banner2-title l2216v1wy4m-editor_css',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <p>
+                      <b>And God said, let there be Fu, and there was Fu.</b>
+                    </p>
+                  </span>
+                </span>
+              </span>
+            </span>
+          ),
+        },
+        content: {
+          className: 'banner2-content',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <p>
+                      <b>上帝说，要有富，于是便有了富。——《旧约:创富记》</b>
+                    </p>
+                  </span>
+                </span>
+              </span>
+            </span>
+          ),
+        },
+        button: {
+          className: 'banner2-button l2216n7mb1f-editor_css',
+          children: 'Learn More',
+        },
+      },
+    ],
+  },
+};
+export const Banner02DataSource = {
+  wrapper: { className: 'banner0 l2215cbr1i-editor_css' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title l22150g6rbp-editor_css',
+    children: 'https://s1.ax1x.com/2022/04/16/Lt2CJs.jpg',
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <span>
+        <p>
+          <br />
+        </p>
+      </span>
+    ),
+  },
+  button: {
+    className: 'banner0-button',
+    children: (
+      <span>
+        <span>
+          <p>
+            <b>爱富 信富 等富</b>
+          </p>
+        </span>
+      </span>
+    ),
+  },
+};
+export const Content131DataSource = {
+  OverPack: {
+    className: 'home-page-wrapper content13-wrapper l223hamdswc-editor_css',
+    playScale: 0.3,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image l223i1twq17-editor_css',
+      },
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <b>
+                      <p>His love for us is like an ocean.</p>
+                      <p>You can see its beginning,</p>
+                      <p>but not its end.</p>
+                    </b>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'title-h1 l223hk6hle-editor_css',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <p>富爱如滔滔江水</p>
+                  <p>延绵不绝</p>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'title-content l223pia68jk-editor_css',
+      },
+      {
+        name: 'content2',
+        children: (
+          <span>
+            <p>
+              <br />
+            </p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+    ],
+  },
+};
